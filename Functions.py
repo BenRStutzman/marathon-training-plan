1a2a29e61a295d5589bfe3c7d3fbac80bb063958
Ben Stutzman <33126601+BenRStutzman@users.noreply.github.com>
2017-11-28 14:32:44 -0500

    Added functions for calculating number of weeks

--- Functions.py
+++ Functions.py
@@ -1,16 +1,25 @@
-""" This file contains the functions for the main method of the marathon training
-plan generator. """
+""" This file contains the functions for the marathon
+training plan generator. """
 
-import datetime as d
 import calendar as c
+import datetime as d
 import easygui as e
+from random import randint
+
+class QuitError(Exception):
+    """ An error to be raised if the user selects 'cancel' in any input box. """
+    pass
 
 def get_date(prompt, min_date, max_date):
-    
+    """ Ask the user to enter a date between two specified dates using
+    easygui."""
     question = 'Please select the year:'
     choices = [i for i in range(min_date.year, max_date.year + 1)]
-    year = int(e.choicebox(question, prompt, choices))
-
+    year = e.choicebox(question, prompt, choices)
+    if year == None:
+        raise QuitError
+    else:
+        year = int(year)
     question = 'Please select the month:'
     choices = [('0' + str(i))[-2:] for i in range(1, 13)]
     if min_date.year == max_date.year:
@@ -19,8 +28,11 @@ def get_date(prompt, min_date, max_date):
         choices = choices[min_date.month - 1:]
     elif year == max_date.year:
         choices = choices[:max_date.month]
-    month = int(e.choicebox(question, prompt, choices))
-
+    month = e.choicebox(question, prompt, choices)
+    if month == None:
+        raise QuitError
+    else:
+        month = int(month)
     question = 'Please select the day:'
     month_length = c.monthrange(year, month)[1]
     choices = [('0' + str(i))[-2:] for i in range(1, month_length + 1)]
@@ -30,11 +42,21 @@ def get_date(prompt, min_date, max_date):
         choices = choices[min_date.day - 1:]
     elif (year, month) == (max_date.year, max_date.month):
         choices = choices[:max_date.day]
-    day = int(e.choicebox(question, prompt, choices))
+    day = e.choicebox(question, prompt, choices)
+    if day == None:
+        raise QuitError
+    else:
+        day = int(day)
     
     return d.date(year, month, day)
 
-def get_start_date(today, mar_date):
+def get_race_date(today, time_range):
+    """ Ask the user for the date of their marathon. """
+    max_date = today.replace(year = today.year + time_range)
+    return get_date("When is your marathon?", today, max_date)
+
+def get_start_date(today, race_date):
+    """ Ask the user for the date they started/will start training. """
     question = "When do you want to start training?"
     choices = ['I already did!', 'Today!', 'Ummm, later...']
     answer = e.choicebox(question, '', choices)
@@ -43,9 +65,45 @@ def get_start_date(today, mar_date):
                         today.year - 1), today)
     elif answer == choices[1]:
         return today
+    elif answer == choices[2]:
+        return get_date('When will you start training?', today, race_date)
     else:
-        return get_date('When will you start training?', today, mar_date)
+        raise QuitError
+
+def calc_weeks(start_date, race_date):
+    race_day = race_date.weekday()
+    start_day = start_date.weekday()
+    days_first_week = (7 - start_day) % 7
+    days_last_week = (race_day + 1) % 7
+    num_days = (race_date - start_date).days + 1
+    if start_date.isocalendar()[:2] == race_date.isocalendar()[:2]:
+        days_first_week = num_days
+        days_last_week = 0
+        num_weeks = 0
+    else:
+        num_weeks = (num_days - days_first_week - days_last_week) // 7
+    if days_first_week:
+        num_weeks += 1
+    if days_last_week:
+        num_weeks += 1
+    return (days_first_week, days_last_week, num_weeks)
+
+def get_mileage(min_mileage):
+    question = "How many miles would you like to run per week?"
+    while True:
+        answer = e.integerbox(question)
+        if answer == None:
+            raise QuitError
+        elif answer >= min_mileage:
+            return answer
+        else:
+            e.msgbox("You must run at least %d miles per week." % min_mileage)
+
+def split_week(days, total_mileage):
+    proportions = [randint(1,100) for i in range(days)]
+    multiplier = total_mileage / sum(proportions)
+    return [round(i * multiplier) for i in proportions]
 
 if __name__ == '__main__':
-    print('This file contains the functions for the main method of '
-          'the marathon training plan generator.''')
+    print("This file contains the functions for "
+          "the marathon training plan generator.")
--- Main.py
+++ Main.py
@@ -2,9 +2,15 @@
 
 import Functions as f
 
-today = f.d.date.today()
-mar_date = f.get_date("When is your marathon?", today,
-                      today.replace(year = today.year + 10))
-start_date = f.get_start_date(today, mar_date)
-time_till_race = mar_date - start_date
-print("You'll be training for", time_till_race.days, "days.")
+try:
+    today = f.d.date.today()
+    race_date = f.get_race_date(today, 10)
+    start_date = f.get_start_date(today, race_date)
+    weekly_mileage = f.get_mileage(10)
+    (days_first_week, days_last_week, plan) = f.create_plan_outline(start_date,
+                                                                    race_date)
+    if days_first_week:
+        plan[1] = f.split_week(days_first_week, weekly_mileage)
+    print(plan)
+except f.QuitError:
+    f.e.msgbox("Too bad. I'm sure you could have done it!")
