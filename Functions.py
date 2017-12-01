826fba87f59e0f428471a0a432b1dcff1b32f72b
Ben Stutzman <33126601+BenRStutzman@users.noreply.github.com>
2017-12-01 01:11:51 -0500

    Added comments and revisions, and an example plan

--- Functions.py
+++ Functions.py
@@ -9,19 +9,19 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
-
 import smtplib
+from smtplib import socket
 
 class QuitError(Exception):
     """ An error to be raised if the user selects 'cancel' in any input box. """
     pass
 
-def get_date(prompt, min_date, max_date):
+def get_date(prompt, title, min_date, max_date):
     """ Ask the user to enter a date between two specified dates using
     easygui."""
-    question = 'Please select the year:'
+    question = prompt + ' Please select the year:'
     choices = [i for i in range(min_date.year, max_date.year + 1)]
-    year = e.choicebox(question, prompt, choices)
+    year = e.choicebox(question, title, choices)
     if year == None:
         raise QuitError
     else:
@@ -34,7 +34,7 @@ def get_date(prompt, min_date, max_date):
         choices = choices[min_date.month - 1:]
     elif year == max_date.year:
         choices = choices[:max_date.month]
-    month = e.choicebox(question, prompt, choices)
+    month = e.choicebox(question, title, choices)
     if month == None:
         raise QuitError
     else:
@@ -48,7 +48,7 @@ def get_date(prompt, min_date, max_date):
         choices = choices[min_date.day - 1:]
     elif (year, month) == (max_date.year, max_date.month):
         choices = choices[:max_date.day]
-    day = e.choicebox(question, prompt, choices)
+    day = e.choicebox(question, title, choices)
     if day == None:
         raise QuitError
     else:
@@ -58,24 +58,51 @@ def get_date(prompt, min_date, max_date):
 def get_race_date(today, time_range):
     """ Ask the user for the date of their marathon. """
     max_date = today.replace(year = today.year + time_range)
-    return get_date("When is your marathon?", today, max_date)
+    return get_date("When is your marathon?", "Race Date", today, max_date)
 
 def get_start_date(today, race_date):
     """ Ask the user for the date they started/will start training. """
+    title = "Starting Date"
     question = "When do you want to start training?"
     choices = ['I already did!', 'Today!', 'Ummm, later...']
-    answer = e.choicebox(question, '', choices)
-    if answer == choices[0]:
-        return get_date('When did you start training?', today.replace(year =
-                        today.year - 1), today)
-    elif answer == choices[1]:
+    choice = e.choicebox(question, title, choices)
+    if choice == choices[0]:
+        return get_date('When did you start training?', title,
+                        today.replace(year = today.year - 1), today)
+    elif choice == choices[1]:
         return today
-    elif answer == choices[2]:
-        return get_date('When will you start training?', today, race_date)
+    elif choice == choices[2]:
+        return get_date('When will you start training?', title, today, race_date)
     else:
         raise QuitError
 
-def get_mileage(prompt, min_mileage, max_mileage, qualifier = ''):
+def calc_schedule(start_date, race_date):
+    """ Calculate the total number of days to train, as well as the number of
+    days in each of the first and last weeks. """
+    race_day = race_date.weekday()
+    start_day = start_date.weekday()
+    num_days = (race_date - start_date).days + 1
+    if start_date.isocalendar()[:2] == race_date.isocalendar()[:2]:
+        days_first_week = num_days
+        days_last_week = num_days
+        num_weeks = 1
+    else:
+        days_first_week = (7 - start_day) % 7
+        days_last_week = (race_day + 1) % 7
+        num_weeks = (num_days - days_first_week - days_last_week) // 7
+        if days_first_week:
+            num_weeks += 1
+        else:
+            days_first_week = 7
+        if days_last_week:
+            num_weeks += 1
+        else:
+            days_last_week = 7
+    return (days_first_week, days_last_week, num_weeks, num_days)
+
+def get_mileage(prompt, title, min_mileage, max_mileage, qualifier = ''):
+    """ Ask the user to enter the number of miles he/she would like to run over
+    a specified time frame. """
     if min_mileage == 1:
         plural1 = ''
     else:
@@ -85,23 +112,29 @@ def get_mileage(prompt, min_mileage, max_mileage, qualifier = ''):
     else:
         plural2 = 's'
     while True:
-        answer = e.integerbox(prompt, lowerbound = None, upperbound = None)
-        if answer == None:
+        entry = e.integerbox(prompt, title, lowerbound = None, upperbound = None)
+        if entry == None:
             raise QuitError
-        elif answer < min_mileage:
+        elif entry < min_mileage:
             e.msgbox("You must run at least %d mile%s%s."
                      % (min_mileage, plural1, qualifier))
-        elif answer > max_mileage:
+        elif entry > max_mileage:
             e.msgbox("You may only run up to %d mile%s%s."
                      % (max_mileage, plural2, qualifier))
         else:
-            return answer
+            return entry
             
             
 def calc_mileage(num_weeks, days_first_week, days_last_week):
-    initial_mileage = get_mileage("Initial miles per week?", 1, 75,
+    """ Create a list of the total number of miles to run each week of
+    training, gradually increasing until shortly before the end. """
+    initial_mileage = get_mileage("How many miles per week would you like to "
+                                  "run at the start of your training?",
+                                  "Initial Miles Per Week", 1, 75,
                                   " per week to start")
-    final_mileage = get_mileage("Final miles per week?", max(30, initial_mileage)
+    final_mileage = get_mileage("How many miles per week would you like to run "
+                                "by the end of your training?", "Final Miles "
+                                "Per Week", max(30, initial_mileage)
                                 , 100, " per week at the end")
     weekly_mileage = []
     if num_weeks == 2:
@@ -124,30 +157,9 @@ def calc_mileage(num_weeks, days_first_week, days_last_week):
     weekly_mileage += [round(final_mileage / 2 * ((days_last_week - 1) / 7))]
     return (initial_mileage, final_mileage, weekly_mileage)
 
-def calc_schedule(start_date, race_date):
-    race_day = race_date.weekday()
-    start_day = start_date.weekday()
-    num_days = (race_date - start_date).days + 1
-    if start_date.isocalendar()[:2] == race_date.isocalendar()[:2]:
-        days_first_week = num_days
-        days_last_week = num_days
-        num_weeks = 1
-    else:
-        days_first_week = (7 - start_day) % 7
-        days_last_week = (race_day + 1) % 7
-        num_weeks = (num_days - days_first_week - days_last_week) // 7
-        if days_first_week:
-            num_weeks += 1
-        else:
-            days_first_week = 7
-        if days_last_week:
-            num_weeks += 1
-        else:
-            days_last_week = 7
-    return (days_first_week, days_last_week, num_weeks, num_days)
-
-
 def split_week(days, total_mileage, spread):
+    """ Randomly split a week into a certain number of miles each day so that
+    the sum is a specified amount, then rebalance the list to avoid outliers. """
     if days == 0:
         return []
     proportions = [randint(1,100) for i in range(days - 1)]
@@ -167,6 +179,8 @@ def split_week(days, total_mileage, spread):
     return mileage
 
 def build_plan(num_weeks, weekly_mileage, days_first_week, days_last_week):
+    """ Create a training plan that tells the user how many miles to run each
+    day from the start date to the race date. """
     plan = []
     spread = 2
     if num_weeks > 1:
@@ -179,6 +193,8 @@ def build_plan(num_weeks, weekly_mileage, days_first_week, days_last_week):
 
 def calc_long_runs(num_weeks, initial_mileage, days_first_week, days_last_week,
                    longest_run):
+    """ Create a list of the longest runs each week, gradually increasing until
+    shortly before the end. """
     long_runs = []
     if days_last_week >= 6:
         last_run = round(longest_run / 2)
@@ -212,6 +228,8 @@ def calc_long_runs(num_weeks, initial_mileage, days_first_week, days_last_week,
     return long_runs + [0]
 
 def insert_long_run(week, week_index, distance):
+    """ Place a long run on the second-to-last day of the week, and rebalance
+    the other days so the total milage stays the same. """
     while week[-2] < distance:
         week[-2] += 1
         second_index = week.index(max(week[:-2]))
@@ -224,8 +242,10 @@ def insert_long_run(week, week_index, distance):
 
 def add_long_runs(plan, num_weeks, initial_mileage, final_mileage,
                   days_first_week, days_last_week):
+    """ Insert long runs into the plan, maintaining the original mileage per
+    week. """
     longest_run = get_mileage("How many miles will your longest training "
-                              "run be?", 10, final_mileage * 2/5,
+                              "run be?", "Longest Run", 10, final_mileage * 2/5,
                               " for your longest run")
     long_runs = calc_long_runs(num_weeks, initial_mileage, days_first_week,
                                days_last_week, longest_run)
@@ -236,6 +256,8 @@ def add_long_runs(plan, num_weeks, initial_mileage, final_mileage,
     return plan
 
 def add_taper(plan, num_days, days_last_week):
+    """ Make the last few days of training shorter, so the runner can rest up
+    for the big race. """
     taper_vals = [5,3,1]
     if num_days >= 2:
         if  days_last_week >= 2:
@@ -261,6 +283,8 @@ def add_taper(plan, num_days, days_last_week):
     return plan
 
 def write_plan(plan, start_date):
+    """ Write the training plan to a text file, including the number of miles
+    to run each day and weekly totals. """
     text = 'Marathon Training Plan'
     counter = 0
     for week_num, week in enumerate(plan):
@@ -281,10 +305,12 @@ def write_plan(plan, start_date):
         else:
             plural = 's'
         text += '\nTotal:' + ' '*20 + str(sum(week)) + ' mile' + plural
+    text += '\n\nYou got this!'
     with open('training_plan.txt', 'w+') as f:
         f.write(text)
 
 def email_plan():
+    """ Email the training plan text file to the user as an attachment. """
     while True:
         address = e.enterbox("Please enter your email address: ")
         if address == None:
@@ -316,19 +342,32 @@ def email_plan():
             server.quit()
             return True
         except smtplib.SMTPRecipientsRefused:
-            if not e.ynbox("Sorry, that email address is invalid. "
-                       "Try another address?"):
-                return False
+            answer = e.ynbox("Sorry, that email address is invalid. "
+                       "Try another address?")
+        except socket.gaierror:
+            answer = e.ynbox("Sorry, the email failed to send (Perhaps no internet "
+                        "connection?). Try again?")
+        if answer == None:
+            raise QuitError
+        elif answer == False:
+            return False
 
 def deliver_plan():
-    if e.ynbox("Your plan has been created! "
-                 "Would you like the file to be emailed to you?"):
+    """ Ask the user if he/she wants to be emailed the training plan. If not,
+    return False so the user can be informed of the text file. """
+    answer = e.ynbox("Your plan has been created! "
+                 "Would you like the file to be emailed to you?")
+    if answer == None:
+        raise QuitError
+    elif answer:
         emailed = email_plan()
     else:
         emailed = False
     return emailed
 
-def ask_another(emailed):
+def conclude(emailed):
+    """ Tell the user if the email was successful, and ask if he/she wants to
+    create another training plan. """
     if emailed:
         addition = "Message sent! "
     else:
