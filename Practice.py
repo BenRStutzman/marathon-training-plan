e54d96ffc8ea5ea8de37f7722900d017511d3ea6
Ben Stutzman <33126601+BenRStutzman@users.noreply.github.com>
2017-11-30 23:32:15 -0500

    Delete Practice.py

--- Practice.py
+++
@@ -1,6 +0,0 @@
-import Functions as f
-
-min_date = f.d.date(1998, 10, 6)
-max_date = f.d.date(2020, 5, 1)
-date = f.get_date('Enter a date between your birth and graduation', min_date, max_date)
-print(date)
